d8d4c6b1e434df12ad75a199cf1e98bc8619eab3
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-05-02 17:47:55 -0300

    declaring variables and size in memory PT01

--- src/main/java/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -4,6 +4,14 @@ public class Class02PrimitiveTypes {
     public static void main(String[] args){
         // int, double, float, boolean, char, byte, short, long
         int age = 10;
-        System.out.println(age);
+        long bigNumber = 10000;
+        float salaryFloat = 2500;
+        double salaryDouble = 5000;
+        boolean trueValue = true;
+        boolean falseValue = false;
+        short ageShort = 29;
+        byte ageByte = 39;
+        char character = 'W';
+        System.out.println("The age is "+age+" years old");
     }
 }
