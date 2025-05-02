541092b204cde9e3d49cdad3ac941e277128e38b
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-05-02 17:02:39 -0300

    Comments

--- src/main/java/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
+++ src/main/java/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
@@ -1,6 +1,18 @@
 package academy.devdojo.javaoneforall.introduction;
 
+/**
+ * this is the class HelloDevDojo and this is <b>javadoc</b>
+ */
 public class HelloDevDojo {
+    // I can type anything and it will be completely ignored
+    /*
+    this
+    is
+    a
+    multiple
+    line
+    comment
+    */
     public static void main(String[] args){
         System.out.println("Hello world!");
     }
