2ac85ac658002e90cff5c98732ca18cdbc7795f4
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-05-02 17:27:30 -0300

    naming convention for variables

--- src/main/java/academy/devdojo/javaoneforall/introduction/Class01HelloDevDojo.java
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class01HelloDevDojo.java
@@ -1,10 +1,10 @@
 package academy.devdojo.javaoneforall.introduction;
 
 /**
- * this is the class HelloDevDojo and this is <b>javadoc</b>
+ * this is the class Class01HelloDevDojo and this is <b>javadoc</b>
  */
-public class HelloDevDojo {
-    // I can type anything and it will be completely ignored
+public class Class01HelloDevDojo {
+    // I can type anything, and it will be completely ignored
     /*
     this
     is
