4efdd389a7ec721d46d4cfb475053239aa3e21ec
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-06-23 17:12:51 -0300

    Operators pt 02 Relational

--- src/main/java/academy/devdojo/javaoneforall/introduction/Class04Operators.java
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -8,5 +8,23 @@ public class Class04Operators {
         int result = number02 - number01;
 
         System.out.println(result);
+
+        // %
+        int modulo = 20 % 2;
+        System.out.println(modulo);
+
+        // < > <= >= != ==
+        boolean isTenLowerThanTwenty = 10 < 20;
+        boolean isTenLowerOrEqualsThanTwenty = 10 <= 20;
+        boolean isTenGreaterThanTwenty = 10 > 20;
+        boolean isTenGreaterOrEqualsThanTwenty = 10 >= 20;
+        boolean isTenDifferentThanTwenty = 10 != 20;
+        boolean isTenEqualsTwenty = 10 == 20;
+        System.out.println("isTenLowerThanTwenty " + isTenLowerThanTwenty);
+        System.out.println("isTenLowerOrEqualsThanTwenty " + isTenLowerOrEqualsThanTwenty);
+        System.out.println("isTenGreaterThanTwenty " + isTenGreaterThanTwenty);
+        System.out.println("isTenGreaterOrEqualsThanTwenty " + isTenGreaterOrEqualsThanTwenty);
+        System.out.println("isTenDifferentThanTwenty " + isTenDifferentThanTwenty);
+        System.out.println("isTenEqualsTwenty " + isTenEqualsTwenty);
     }
 }
