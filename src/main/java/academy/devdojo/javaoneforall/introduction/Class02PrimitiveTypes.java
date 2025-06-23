ea4a4f1b2ba5d98b7f425730e1246eb17b15ca82
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-06-23 16:03:59 -0300

    Class02PrimitiveTypes pt 02 declaring variables and size in memory

--- src/main/java/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -5,13 +5,14 @@ public class Class02PrimitiveTypes {
         // int, double, float, boolean, char, byte, short, long
         int age = 10;
         long bigNumber = 10000;
-        float salaryFloat = 2500;
-        double salaryDouble = 5000;
+        float salaryFloat = 2500.0F;
+        double salaryDouble = 5000.0D;
         boolean trueValue = true;
         boolean falseValue = false;
         short ageShort = 29;
-        byte ageByte = 39;
+        byte ageByte = -128;
         char character = 'W';
+        char characterAscii = '\u0040';
         System.out.println("The age is "+age+" years old");
     }
 }
