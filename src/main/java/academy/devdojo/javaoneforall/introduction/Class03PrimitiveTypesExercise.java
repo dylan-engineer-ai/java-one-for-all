4da4246ff3e1d216caad22ddc363fe6c072e5515
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-06-23 16:40:03 -0300

    Primitive types pt 05 Exercise

---
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class03PrimitiveTypesExercise.java
@@ -0,0 +1,15 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class03PrimitiveTypesExercise {
+    public static void main(String[] args){
+        String name = "M4ver1ck";
+        String address = "South city";
+        // dd/MM/yyyy
+        String salaryDateReceived = "23/06/2025";
+        double salary = 3300.99D;
+        System.out.println("I " + name +
+                " living at " + address +
+                " confirm that I received the salary of " + salary +
+                " on "+ salaryDateReceived);
+    }
+}
