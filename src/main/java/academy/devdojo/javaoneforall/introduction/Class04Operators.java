b2f68a979370de17be7891fdb69293cff9a2adce
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-06-23 17:00:31 -0300

    Operators pt 01 Arithmetic

---
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -0,0 +1,12 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class04Operators {
+    public static void main(String[] args) {
+        // + - / *
+        int number01 = 10;
+        int number02 = 20;
+        int result = number02 - number01;
+
+        System.out.println(result);
+    }
+}
