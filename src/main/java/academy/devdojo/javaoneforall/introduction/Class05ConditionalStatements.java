ed754914a645a3617c056f699b5f64f5b2f04fa5
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-07-31 15:17:41 -0300

    Conditional Statement pt 01 - IF

---
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class05ConditionalStatements.java
@@ -0,0 +1,21 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class05ConditionalStatements {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean isAllowedToBuyAlcohol = age >= 18;
+        if (isAllowedToBuyAlcohol){
+            System.out.println("Authorized to buy alcohol");
+        }
+
+        if(!isAllowedToBuyAlcohol){
+            System.out.println("Sorry, you are not allowed to buy alcohol");
+        }
+
+        boolean c = true;
+        if(c == false){
+            System.out.println("Insid weird condition");
+        }
+        System.out.println("Outside IF");
+    }
+}
