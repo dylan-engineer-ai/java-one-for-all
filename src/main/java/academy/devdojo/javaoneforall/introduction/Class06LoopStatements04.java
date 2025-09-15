41dd16cac3ea0cf94ba68cd17db6f4449455921c
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-09-15 12:06:58 -0300

    Loop pt 03 continue

---
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class06LoopStatements04.java
@@ -0,0 +1,14 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class06LoopStatements04 {
+    public static void main(String[] args) {
+        double totalPrice = 20000;
+        for (int monthCount = (int) totalPrice; monthCount >= 1; monthCount--) {
+            double monthlyPrice = totalPrice / monthCount;
+            if(monthlyPrice < 1000){
+                continue;
+            }
+            System.out.println("Months "+monthCount+" Price: " + monthlyPrice);
+        }
+    }
+}
