85c425af3cd868875608fec12cc8fc970965b2b7
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-09-15 11:57:56 -0300

    Loop pt 03 exercise break

--- src/main/java/academy/devdojo/javaoneforall/introduction/Class06LoopStatements03.java
+++ src/main/java/academy/devdojo/javaoneforall/introduction/Class06LoopStatements03.java
@@ -2,6 +2,14 @@ package academy.devdojo.javaoneforall.introduction;
 
 public class Class06LoopStatements03 {
     public static void main(String[] args) {
-
+        double totalPrice = 20000;
+        for (int monthCount = 0; monthCount <= totalPrice; monthCount++) {
+            double monthlyPrice = totalPrice / monthCount;
+            if(monthlyPrice < 1000){
+                break;
+            }
+            System.out.println("Months "+monthCount+" Price: " + monthlyPrice);
+            System.out.println("Inside for, Outside if"+monthCount);
+        }
     }
 }
