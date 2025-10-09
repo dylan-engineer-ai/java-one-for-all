94daea1c11623e6edaebb119d9c85c6f01cda192
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-10-09 16:47:08 -0300

    Object Orientation - Methods pt 02 - Parameters

--- src/main/java/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
@@ -4,4 +4,9 @@ public class Calculator {
     public void sumTwoNumbers(){
         System.out.println(10+10);
     }
+
+
+    public void multiplyTwoNumbers(int num1, int num2){
+        System.out.println(num1*num2);
+    }
 }
