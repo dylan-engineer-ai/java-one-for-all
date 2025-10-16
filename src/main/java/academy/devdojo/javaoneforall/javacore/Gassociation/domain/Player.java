3506d114e438858ceca626cf458e96a38ae700b3
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-10-16 10:50:17 -0300

    Object Orientation - Association pt 02 - Unidirectional association one to many

--- src/main/java/academy/devdojo/javaoneforall/javacore/Gassociation/domain/Player.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/Gassociation/domain/Player.java
@@ -3,8 +3,21 @@ package academy.devdojo.javaoneforall.javacore.Gassociation.domain;
 public class Player {
     private String name;
 
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
     public void print(){
         System.out.println(this.name);
+        if(this.team != null) {
+            System.out.println(this.team.getName());
+        }
     }
 
     public String getName() {
