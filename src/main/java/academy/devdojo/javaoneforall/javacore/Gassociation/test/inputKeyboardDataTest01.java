901e97a2cefe7fbe24a30d0ab9f948c856ddcd94
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-10-16 11:36:05 -0300

    Object Orientation - Reading data from the keyboard

---
+++ src/main/java/academy/devdojo/javaoneforall/javacore/Gassociation/test/inputKeyboardDataTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.javaoneforall.javacore.Gassociation.test;
+
+import java.util.Scanner;
+
+public class inputKeyboardDataTest01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please type your full name");
+        String name = input.nextLine();
+        System.out.println("PLease type your age");
+        int age = input.nextInt();
+        System.out.println("Please type M or F for you gender");
+        char gender = input.next().charAt(0);
+
+        System.out.println("-------------------");
+        System.out.println("Name " + name);
+        System.out.println("Age " + age);
+        System.out.println("Gender " + gender);
+
+    }
+}
