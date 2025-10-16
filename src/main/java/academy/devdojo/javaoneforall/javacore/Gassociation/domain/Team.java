c5c21726812f3891fb3dd5190c5e0fe473e14e51
m4ver1ck-droid <m4ver1ck.droid@gmail.com>
2025-10-16 11:09:01 -0300

    Object Orientation - Association pt 04 - Bidirectional association

--- src/main/java/academy/devdojo/javaoneforall/javacore/Gassociation/domain/Team.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/Gassociation/domain/Team.java
@@ -2,9 +2,23 @@ package academy.devdojo.javaoneforall.javacore.Gassociation.domain;
 
 public class Team {
     private String name;
+    private Player[] players;
 
     public String getName() {
         return name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public Team(String name, Player[] players) {
+        this.name = name;
+        this.players = players;
     }
 
     public void setName(String name) {
@@ -13,5 +27,14 @@ public class Team {
 
     public Team(String name) {
         this.name = name;
+    }
+
+    public void print(){
+        System.out.println(this.name);
+        if (players == null) return;
+        for (Player player : players) {
+            System.out.println(player.getName());
+        }
+
     }
 }
---
+++ src/main/java/academy/devdojo/javaoneforall/javacore/Gassociation/test/PlayerTest03.java
@@ -0,0 +1,24 @@
+package academy.devdojo.javaoneforall.javacore.Gassociation.test;
+
+import academy.devdojo.javaoneforall.javacore.Gassociation.domain.Player;
+import academy.devdojo.javaoneforall.javacore.Gassociation.domain.Team;
+
+public class PlayerTest03 {
+    public static void main(String[] args) {
+        Player player = new Player("Pele");
+        Player player2 = new Player("Cafu");
+        Team team = new Team("Brazil's national team");
+        Player[] players = {player,player2};
+
+        player.setTeam(team);
+        player2.setTeam(team);
+        team.setPlayers(players);
+
+
+        System.out.println("------------ Player -----------------");
+        player.print();
+
+        System.out.println("------------ Team -----------------");
+        team.print();
+    }
+}
